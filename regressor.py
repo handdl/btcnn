726b0e1f3515f450f5563d603ac1d92ab213a790
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-14 12:35:18 +0700

    move architecture to how to notebook

--- regressor.py
+++ regressor.py
@@ -1,12 +1,6 @@
 import torch
 from torch import nn, Tensor
-from layers import (
-    BinaryTreeSequential,
-    BinaryTreeActivation,
-    BinaryTreeConv,
-    BinaryTreeInstanceNorm,
-    BinaryTreeAdaptivePooling,
-)
+from layers import BinaryTreeSequential
 
 
 class BinaryTreeRegressor(nn.Module):
@@ -27,35 +21,3 @@ class BinaryTreeRegressor(nn.Module):
 
     def forward(self, vertices: "Tensor", edges: "Tensor") -> "Tensor":
         return self.fcnn(self.btcnn(vertices=vertices, edges=edges))
-
-
-def get_big_btcnn_and_instance_norm(in_channels: "int") -> "BinaryTreeSequential":
-    return BinaryTreeSequential(
-        BinaryTreeConv(in_channels, 64),
-        BinaryTreeActivation(torch.nn.functional.leaky_relu),
-        BinaryTreeConv(64, 128),
-        BinaryTreeInstanceNorm(128),
-        BinaryTreeActivation(torch.nn.functional.leaky_relu),
-        BinaryTreeConv(128, 256),
-        BinaryTreeInstanceNorm(256),
-        BinaryTreeActivation(torch.nn.functional.leaky_relu),
-        BinaryTreeConv(256, 512),
-        BinaryTreeAdaptivePooling(torch.nn.AdaptiveMaxPool1d(1)),
-    )
-
-
-def get_big_fcnn() -> "nn.Sequential":
-    return nn.Sequential(
-        nn.Linear(512, 256),
-        nn.LeakyReLU(),
-        nn.Linear(256, 128),
-        nn.LeakyReLU(),
-        nn.Linear(128, 64),
-        nn.LeakyReLU(),
-        nn.Linear(64, 1),
-        nn.Softplus(),
-    )
-
-
-def get_bt_regressor(in_channels: "int", name: "str", device: "torch.device") -> "BinaryTreeRegressor":
-    return BinaryTreeRegressor(get_big_btcnn_and_instance_norm(in_channels), get_big_fcnn(), name=name, device=device)
