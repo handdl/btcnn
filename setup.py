ebbdcbd1905287d7ce67eb9dc92d011fba7a8ab4
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-30 17:10:09 +0700

    fix black issue in setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ def parse_requirements(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
+
 setup(
     name="btcnn",
     version="0.1",
