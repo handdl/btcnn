9c7b5d84768ed2bcb469da099420fdc1d60649b4
Sergey <58306690+zinchse@users.noreply.github.com>
2024-08-30 15:59:23 +0700

    rm pragma

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 
-# pragma: no cover
 def parse_requirements(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
